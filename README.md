4e7a729d7c2418e13043a356ebdfa6eaba26c1dc
Roseak-lin <roseak.lin@gmail.com>
2021-06-26 10:48:33 -0400

    Updated files and README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Repository for the Earth Observing Dashboard hackathon
+
+Collaborative project created by:
+- Roseak Lin (https://github.com/Roseak-lin)
+- Daniel Sun (https://github.com/UnripedBanana)
+- Kevin Kim (https://github.com/kevinkim31)
+- Ariyan Haq
---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Info goes here</p>
+      </div>
+    );
+  }
+}
+
